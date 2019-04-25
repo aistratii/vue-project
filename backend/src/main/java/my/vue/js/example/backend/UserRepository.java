88cd957cacb03dd9fa179882a7b9f5868d698a19
Alexandru ISTRATII <aistratii@pentalog.com>
2019-04-25 10:20:11 +0300

    functional back and front end

---
+++ backend/src/main/java/my/vue/js/example/backend/UserRepository.java
@@ -0,0 +1,14 @@
+package my.vue.js.example.backend;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+
+    List<User> findByLastName(@Param("lastname") String lastname);
+
+    List<User> findByFirstName(@Param("firstname") String firstname);
+
+}
